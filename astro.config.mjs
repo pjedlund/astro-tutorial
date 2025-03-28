43a9f9ea3659273fef7888562539775a0780da9f
Johan Edlund <me@johanedlund.se>
2025-03-28 20:27:21 +0100

    preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://calm-llama-8c2852.netlify.app"
+  site: "https://calm-llama-8c2852.netlify.app",
+  integrations: [preact()]
 });
-
